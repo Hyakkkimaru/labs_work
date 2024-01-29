44be454f005b8bb6ac29a5067d955e27cbe8550e
Hyakkkimaru <129628826+Hyakkkimaru@users.noreply.github.com>
2024-01-29 10:31:30 +0300

    Create file Insertion_sort.cpp

---
+++ С++/C++_Basic/lab_1/Insertion_sort.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+void insertion_sort(std::vector<int>& arr) {
+    int n = arr.size();
+
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
+}
+
+int main() {
+    std::vector<int> arr = {10, 3, 5, 4, 1};
+
+    insertion_sort(arr);
+
+    std::cout << "Sorted array: ";
+    for (int i = 0; i < arr.size(); i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << "\n";
+
+    return 0;
+}
